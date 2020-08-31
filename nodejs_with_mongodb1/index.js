5226cb2277949ae8dfd04b4d87541397e2a498d8
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2020-08-31 22:57:53 +0530

    comment mongodb access

--- nodejs_with_mongodb1/index.js
+++ nodejs_with_mongodb1/index.js
@@ -1,1 +1,11 @@
 const express=require("express");
+
+const app=express();
+
+const mongourl="mongodb+srv://herath:herath41@cluster0.kl6b4.azure.mongodb.net/<dbname>?retryWrites=true&w=majority";
+
+const PORT=process.env.PORT||5001;
+
+app.listen(PORT,()=>{
+    console.log(`server started at ${PORT}`);
+});
