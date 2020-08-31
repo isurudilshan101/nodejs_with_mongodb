d2ad78ac7a8c4af389832d70adc73020b3f39625
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2020-08-31 23:15:52 +0530

    comment mongodb connect

--- nodejs_with_mongodb1/index.js
+++ nodejs_with_mongodb1/index.js
@@ -1,8 +1,17 @@
 const express=require("express");
+const mongoose=require("mongoose");
 
 const app=express();
 
 const mongourl="mongodb+srv://herath:herath41@cluster0.kl6b4.azure.mongodb.net/<dbname>?retryWrites=true&w=majority";
+
+const connectDB= async()=>{
+    try {
+        await mongoose.connect(mongourl);
+    } catch (err) {
+        console.log (err.message);
+    }
+}
 
 const PORT=process.env.PORT||5001;
 
